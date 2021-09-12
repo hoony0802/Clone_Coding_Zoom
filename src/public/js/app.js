d6f7a65b5555c4f7793219dcd249f16380bb5793
hoony0802 <hoony0802@naver.com>
2021-09-13 02:08:39 +0900

    Complete WebSocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,50 @@
-alert("hi");
+//  * 서버와의 연결
+const socket = new WebSocket(`ws://${window.location.host}`);
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server");
+});
+
+// * 서버로부터 메세지 받음
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("DisConnected from Server");
+});
+
+// * 서버로 메세지 전송
+// setTimeout(() => {
+//   socket.send("hello from the Browser!");
+// }, 10000);
+
+// * 서버와 통신하기 위해 객체로 변환 후 JSON으로 전송
+const makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
+
+const handleNickSubmit = (event) => {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  const li = document.createElement("li");
+  li.innerText = `You: ${input.value}`;
+  messageList.append(li);
+  input.value = "";
+};
+
+nickForm.addEventListener("submit", handleNickSubmit);
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -9,4 +11,38 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+//  * http,WebSocket 둘 다 실행
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+//  * 브라우저와의 연결
+// * 이벤트 함수와 형태 비슷
+wss.on("connection", (socket) => {
+  // * 메세지는 sockets 배열로
+  sockets.push(socket);
+  // * 닉네임은 socket 객체의 nickname 키값 바뀔때마다 갱신
+  socket.nickname = "Anon";
+  console.log("Connected to Browser");
+  socket.on("close", () => console.log("Disconnected from the Browser"));
+  socket.on("message", (msg) => {
+    // * 받은 JSON파일을 객체로 변환
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+
+      case "nickname":
+        socket.nickname = message.payload;
+        break;
+    }
+  });
+});
+
+server.listen(3000, handleListen);
